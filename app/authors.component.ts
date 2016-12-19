cce07d09fa2aeff6c71ebc45e7f6646a52040755
cajoue <cajoue@users.noreply.github.com>
2016-12-19 16:49:13 +0100

    feat: add properties to author component

--- app/authors.component.ts
+++ app/authors.component.ts
@@ -2,8 +2,11 @@ import {Component} from 'angular2/core'
 
 @Component({
   selector: 'authors',
-  template: '<h2>Authors</h2>'
+  template: `
+  <h2>Authors</h2>
+  {{ title }}
+  `
 })
 export class AuthorsComponent {
-  
+  title = "The title of the authors page";
 }
