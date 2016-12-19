a2f3b86ee8f260739a30c383314c8c105d0c5f45
cajoue <cajoue@users.noreply.github.com>
2016-12-19 16:56:45 +0100

    feat: show list of hard-coded authors

--- app/authors.component.ts
+++ app/authors.component.ts
@@ -5,8 +5,14 @@ import {Component} from 'angular2/core'
   template: `
   <h2>Authors</h2>
   {{ title }}
+  <ul>
+    <li *ngFor='#author of authors'>
+      {{ author }}
+    </li>
+  </ul>
   `
 })
 export class AuthorsComponent {
   title = "The title of the authors page";
+  authors = ["AuthorA", "AuthorB", "AuthorC"];
 }
